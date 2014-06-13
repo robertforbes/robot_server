3027982845559cddd908bf35f912fc164e7ffe2a
Robert Forbes <robert.forbes@pentlandedge.com>
2014-06-13 22:17:58 +0100

    Updates to link robot_server to the robot drive functions.

--- src/robot_udp.erl
+++ src/robot_udp.erl
@@ -1,5 +1,11 @@
 -module(robot_udp).
--export([start_server/0, start_server/1, display_controls/1, client/2]).
+-export([
+    start_server/0,
+    start_server/1,
+    start_udp_serial_server/1,
+    display_controls/1,
+    client/2,
+    client/3]).
 
 -define(SERVER_HOST, "localhost").
 -define(SERVER_PORT, 1995).
@@ -9,6 +15,13 @@ start_server() ->
 
 start_server(PktFun) ->
     spawn(fun() -> server(?SERVER_PORT, PktFun) end).
+
+start_udp_serial_server(SerialDevice) ->
+    SP = serial:start([{speed,115200},{open,SerialDevice}]),
+    Drive = fun(<<Speed:8, Direction:8>>) ->
+                robot:drive(SP, Speed, Direction)
+            end,
+    spawn(fun() -> server(?SERVER_PORT, Drive) end).
 
 %% The server.
 
@@ -34,9 +47,12 @@ display_controls(<<Speed:8, Direction:8>>) ->
 %% The client
 
 client(Speed, Direction) ->
+    client(?SERVER_HOST, Speed, Direction).
+
+client(Host, Speed, Direction) ->
     {ok, Socket} = gen_udp:open(0, [binary]),
     io:format("Client opened socket: ~p~n", [Socket]),
-    ok = gen_udp:send(Socket, ?SERVER_HOST, ?SERVER_PORT, list_to_binary([Speed, Direction])),
+    ok = gen_udp:send(Socket, Host, ?SERVER_PORT, list_to_binary([Speed, Direction])),
 
     Value = receive
                 {udp, Socket, _, _, Bin} = Msg ->
@@ -47,4 +63,3 @@ client(Speed, Direction) ->
             end,
     gen_udp:close(Socket),
     Value.
-
